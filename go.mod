6070dfee294a1b197cb025f2c017382bfc348587
nikhil478 <nikhilmatta45@gmail.com>
2025-09-21 01:09:18 +0530

    config histogram

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/metric-otel-golang
+
+go 1.25.0
+
+require (
+	go.opentelemetry.io/otel/metric v1.38.0
+	go.opentelemetry.io/otel/sdk v1.38.0
+)
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+)
+
+require (
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
+	go.opentelemetry.io/otel v1.38.0
+	go.opentelemetry.io/otel/exporters/stdout/stdoutmetric v1.38.0
+	go.opentelemetry.io/otel/sdk/metric v1.38.0
+	go.opentelemetry.io/otel/trace v1.38.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"	
+	apimetric "go.opentelemetry.io/otel/metric"	
+)
+
+var meter = otel.Meter("github.com/metric-otel-golang/metric")
+
+func main() {
+
+	res, err := newResource()
+	if err != nil {
+		panic(err)
+	}
+
+	meterProvider, err := newMeterProvider(res)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := meterProvider.Shutdown(context.Background()); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	otel.SetMeterProvider(meterProvider)
+
+	histogram, err := meter.Float64Histogram(
+		"task.duration",
+		apimetric.WithDescription("The duration of task execution."),
+		apimetric.WithUnit("s"),
+	)
+	if err != nil {
+		panic(err)
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		time.Sleep(1000)
+		duration := time.Since(start)
+		histogram.Record(r.Context(), duration.Seconds())
+		histogram.Record(r.Context(), -duration.Seconds())
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func newResource() (*resource.Resource, error) {
+	return resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName("test-service"),
+			semconv.ServiceVersion("0.1.0"),
+		),
+	)
+}
+
+func newMeterProvider(res *resource.Resource) (*metric.MeterProvider, error) {
+	metricExporter, err := stdoutmetric.New()
+	if err != nil {
+		return nil, err
+	}
+
+	meterProvider := metric.NewMeterProvider(
+		metric.WithResource(res),
+		metric.WithReader(metric.NewPeriodicReader(metricExporter,
+			// Default is 1m. Set to 3s for demonstrative purposes.
+			metric.WithInterval(3*time.Second))),
+	)
+	return meterProvider, nil
+}
