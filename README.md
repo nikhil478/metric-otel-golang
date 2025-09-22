4a97d043977da76103771468ec5bc146b525bc69
nikhil478 <nikhilmatta45@gmail.com>
2025-09-22 16:16:42 +0530

    monitoring metrcis

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # metric-otel-golang
+
+
+docker run --rm -it -v $(pwd)/collector-config.yaml:/etc/otelcol/config.yaml -p 4317:4317 -p 4318:4318 otel/opentelemetry-collector:latest --config=/etc/otelcol/config.yaml
+
+
+docker run --rm -it -v $(pwd)/collector-config.yaml:/etc/otelcol/config.yaml -p 4317:4317 -p 4318:4318 otel/opentelemetry-collector:latest --config=/etc/otelcol/config.yaml
--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"	
@@ -40,13 +40,16 @@ func main() {
 		"task.duration",
 		apimetric.WithDescription("The duration of task execution."),
 		apimetric.WithUnit("s"),
+		apimetric.WithExplicitBucketBoundaries(
+        -5, -2, -1, 0, 1, 5, 10, 25, 50, 75, 100, 250, 500, 750, 1000,
+    ),
 	)
 	if err != nil {
 		panic(err)
 	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		time.Sleep(1000)
+		time.Sleep(time.Second*3)
 		duration := time.Since(start)
 		histogram.Record(r.Context(), duration.Seconds())
 		histogram.Record(r.Context(), -duration.Seconds())
@@ -67,7 +70,11 @@ func newResource() (*resource.Resource, error) {
 }
 
 func newMeterProvider(res *resource.Resource) (*metric.MeterProvider, error) {
-	metricExporter, err := stdoutmetric.New()
+	
+	metricExporter, err := otlpmetricgrpc.New(context.Background(),
+    otlpmetricgrpc.WithEndpoint("127.0.0.1:4317"),
+    otlpmetricgrpc.WithInsecure(),
+)
 	if err != nil {
 		return nil, err
 	}
